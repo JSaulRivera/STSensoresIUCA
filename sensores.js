a64f8c1d6340aee49b46f5f4b625c1baa3ac1ea2
js43016 <jsaul.rivera@hotmail.com>
2025-07-28 17:47:22 -0600

    agregar url Dashboard conteo vehiculos

--- sensores.js
+++ sensores.js
@@ -412,7 +412,7 @@ function mostrarDatosSensor(atributos) {
         window.open("https://smart-twins.sigsa.info/portal/apps/dashboards/c549225088234bfda2955f56f9677ad0");
       }
       else if(atributos.nombre=="Camara DH-ECA2A1400-HN"){
-        //window.open("https://smart-twins.sigsa.info/portal/apps/dashboards/c549225088234bfda2955f56f9677ad0");
+        window.open("https://smart-twins.sigsa.info/portal/apps/dashboards/2b23130b44d149d7af04222fd5d947fa");
       }
       else{
         window.open(`datos.html?atributos=${atributosCodificados}`);
